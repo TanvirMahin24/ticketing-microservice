d636f88221b518a939ad04ec4a96844fdec5aafb
Tanvir Mahin <tanvirmahin24@gmail.com>
2023-06-08 20:35:02 +0600

    run on minikube but ingress is inaccessable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,9 @@ app.use(json());
 app.get("/api/users/currentuser", (req, res) => {
   res.send("Hi from auth!");
 });
+app.get("/api/users", (req, res) => {
+  res.send("Hi from auth! testr");
+});
 
 app.listen(3000, () => {
   console.log("[AUTH] Listening on 3000");

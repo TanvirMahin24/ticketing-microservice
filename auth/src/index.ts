025294b4a849ea5bef56a2c52a3c274304119304
Tanvir Mahin <tanvirmahin24@gmail.com>
2023-06-16 14:23:52 +0600

    error genaralized and mongo deployment added

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { currentuserRouter } from "./routes/current-user";
 import { signupRouter } from "./routes/signup";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
 
 const app = express();
 app.use(json());
@@ -12,6 +13,8 @@ app.use(currentuserRouter);
 app.use(signupRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("[AUTH] Listening on 3000");
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,34 @@
 import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 const router = express.Router();
 
-router.post("/api/users/signin", (req: Request, res: Response) => {});
+router.post(
+  "/api/users/signin",
+  [
+    body("email").isEmail().withMessage("Enter a valid email address!"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage(
+        "Enter password with minimum 4 charecters and maximum of 20 charecters!"
+      ),
+  ],
+  (req: Request, res: Response) => {
+    // Validation Result
+    const errors = validationResult(req);
+
+    // Handle error response
+    if (!errors.isEmpty()) {
+      console.log(errors.array());
+      throw new RequestValidationError(errors.array());
+    }
+
+    // Request body data
+    const { email, password } = req.body;
+
+    res.send("Signin");
+  }
+);
 
 export { router as signinRouter };

f6f445f5a6c05a73014e3e5ec628e5d2c00072c6
Tanvir Mahin <tanvirmahin24@gmail.com>
2023-06-22 21:32:16 +0600

    events in nats added with abstract base listener

--- nats-test-project/src/listener.ts
+++ nats-test-project/src/listener.ts
@@ -1,14 +1,22 @@
+import { randomBytes } from "crypto";
 import nats from "node-nats-streaming";
+import { TicketCreatedListener } from "./events/ticket-created-listener";
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", (stream) => {
   console.log("Listener connected to NATS!!!");
 
-  const subscription = stan.subscribe("ticket:created");
-  subscription.on("message", () => {
-    console.log("Message recived");
+  // When NATS client closed
+  stan.on("close", () => {
+    console.log("NATS Client closed!");
+    process.exit();
   });
+
+  new TicketCreatedListener(stan).listen();
 });
+
+process.on("SIGINT", () => stan.close());
+process.on("SIGTERM", () => stan.close());
--- nats-test-project/src/publisher.ts
+++ nats-test-project/src/publisher.ts
@@ -1,6 +1,7 @@
+import { randomBytes } from "crypto";
 import nats from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "abc", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
